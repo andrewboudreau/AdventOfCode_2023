6184247b45062c8a3aa9cbf49eddce81398326c5
Andrew Boudreau <andrewboudreau@gmail.com>
2023-12-05 23:30:52 -0600

    added parsing for day 5

--- Day05/Program.cs
+++ Day05/Program.cs
@@ -1,16 +1,26 @@
 ﻿// https://adventofcode.com/2023/day/5
 
-var almanac = ReadTo<Almanac>(Almanac.Create);
+var almanac = ReadTo(Almanac.Create);
 Console.WriteLine(almanac);
 
 class Almanac
 {
     private readonly int[] seeds;
+    private readonly List<Map> maps;
+    private List<int> ranges;
 
     public Almanac(string seeds)
     {
         this.seeds = ParseIntegers(seeds, skipUntil: ':', thenSplitOn: ' ').ToArray();
+        this.maps = [];
+        this.ranges = [];
     }
+
+    private void StartMap(string source, string destination)
+        => maps.Add(new Map(source, destination));
+
+    private void AddRange(IEnumerable<int> ranges)
+        => this.ranges.AddRange(ranges);
 
     public static Almanac Create(IEnumerable<string?> data)
     {
@@ -18,19 +28,32 @@ class Almanac
         iter.MoveNext();
 
         var almanac = new Almanac(iter.Current!);
-        
-
-        while(iter.MoveNext())
+        while (iter.MoveNext())
         {
-
+            if (iter.Current is null)
+            {
+                break;
+            }
+            else if (iter.Current.Contains(':'))
+            {
+                var part = iter.Current.Split(' ')[0].Split('-');
+                almanac.StartMap(part[0], part[^1]);
+            }
+            else if (iter.Current.Split(' ').Length != 0)
+            {
+                almanac.AddRange(iter.Current.Split(' ').Select(int.Parse));
+            }
         }
-        
+
         return almanac;
     }
 
-    public class Map
+    public class Map(string source, string destination)
     {
-        public string SourceCategory { get; private set; }
-    }
+        public string Source { get; private set; } = source;
 
+        public string Destination { get; private set; } = destination;
+
+        public List<int> Ranges { get; private set; } = new();
+    }
 }
